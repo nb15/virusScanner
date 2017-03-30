24d519a9a1055403e10e0fa16e522427af6525af
Noopur Bhatt <nbhatt2020@pingry.org>
2017-03-30 12:23:01 -0400

    Added if statement that scanned file and prints whether or not file contains virus.

--- virus.py
+++ virus.py
@@ -1,4 +1,9 @@
 userFile = input("What file would you like me to open?: ")
 fread = open(userFile, "r+")
 
+if "virus" in userFile:
+	print("This file contains a virus. Contact help immediatly.")
+else:
+	print("File {} is virus free.".format(userfile))
+
 fread.close()
